5866ff6bdff65fa83fdea0aa7c2a2890f3b7596a
Vortexstbloons <notaspawn@gmail.com>
2026-09-05 11:17:22 -0600

    got rid of  a bnuch of useless test

--- packages/importer/src/identity.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import { canonicalPlayerName } from './identity.ts';
-describe('canonicalPlayerName', () => {
-  it('leaves unrelated source names unchanged', () => {
-    expect(canonicalPlayerName('1', 'Alpha', 'Ace')).toEqual(['Alpha', 'Ace']);
-  });
-});
